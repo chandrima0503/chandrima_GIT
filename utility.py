34f0e0e37688a3a893bf17f0b37cc590e0ccba6b
chandrima0503 <chandrimam06@gmail.com>
2022-07-07 11:58:18 +0530

    Update utility.py
    Utility file for python IO

--- utility.py
+++ utility.py
@@ -8,26 +8,28 @@ if command == "-l":
     for line in file:
         num_lines += 1
     print(num_lines, fname)
-if command == '-w':
+elif command == '-w':
     words = data.split()
     print(len(words), fname)
-if command == "-c":
+elif command == "-c":
     no_of_chars = len(data)
     print(no_of_chars, fname)
-if command == "-n":
+elif command == "-n":
     for line in file:
         words = line.split()
         for i in words:
             for letter in i:
                 if(letter.isdigit()):
                     print(letter, end=" ")
-if command == "-a":
+elif command == "-a":
     for line in file:
         words = line.split()
         for i in words:
             for letter in i:
                 if(letter.isalpha()):
                     print(letter, end=" ")
-if command == '-h':
+elif command == '-h':
     print("-l: should display no. of lines present in a file \n -c: no. of characters present in a file \n -w: no. of words in a file \n -n: should display only numeric charcters in input file \n -a: should display only alphabets in input file \n -h: help message to run your program")
+else:
+    print("Wrong option selected. Please do -h to check for available options")
 
